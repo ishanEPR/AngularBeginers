9ed391bdf1a5ca774f5e57e29766686517362e26
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-08-31 16:54:54 +0530

    add form using angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
 import { FormComponent } from './form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormAngularMaterialComponent } from './form-angular-material/form-angular-material.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     SecondComponentComponent,
-    FormComponent
+    FormComponent,
+    FormAngularMaterialComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-angular-material/form-angular-material.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-angular-material',
+  templateUrl: './form-angular-material.component.html',
+  styleUrls: ['./form-angular-material.component.css']
+})
+export class FormAngularMaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
