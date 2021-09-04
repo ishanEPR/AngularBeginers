5abb653f5037355e677142c16686d8b38db76c70
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-09-04 11:55:50 +0530

    display dummy data

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Employee{
+
+  id: String;
+  name:String;
+  email:String;
+  password: String;
+
+
+}
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+
+  employees: Employee[]=[
+    {id:'1',name:'ishan',email:'ishan@gmail.com',password:'123'},
+    {id:'2',name:'is',email:'is@gmail.com',password:'123'},
+    {id:'3',name:'reshmika',email:'resh@gmail.com',password:'123'},
+  ];
 
   constructor() { }
 
